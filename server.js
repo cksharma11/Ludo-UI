8ec8d346b5af1b424780d752c5e454cab3181a14
Chandan Kumar <22887527+cksharma11@users.noreply.github.com>
2019-06-15 15:26:45 +0530

    <CREATE-JOIN> Added Server

--- server.js
+++ server.js
@@ -1,14 +1,30 @@
+const Next = require('next');
 const fastify = require('fastify')({ logger: true });
+const routes = require('./routes/routes');
 
-const PORT = process.env.PORT || 8080;
+const PORT = process.env.PORT || 3033;
+const dev = process.env.NODE_ENV !== 'production';
 
-fastify.get('/', (req, res) => {
-  res.send('Hello Hello!');
+fastify.register((plugin, options, next) => {
+  const app = Next({ dev });
+  const handler = routes.getRequestHandler(app);
+
+  app
+    .prepare()
+    .then(() => {
+      plugin.get('/*', async (req, reply) => {
+        handler(req.req, reply.res);
+        reply.sent = true; // eslint-disable-line no-param-reassign
+      });
+
+      next();
+    })
+    .catch((err) => next(err));
 });
 
 const start = () => {
-  fastify.listen(PORT);
-  fastify.log.info(`Server listening on ${PORT}`);
+  // eslint-disable-next-line no-console
+  fastify.listen(PORT, () => console.log(`server is running on ${PORT}`));
 };
 
 start();
