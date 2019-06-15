76d2ce1fd24efa4082d5873c6ec7e969d715851f
Chandan Kumar <22887527+cksharma11@users.noreply.github.com>
2019-06-15 11:46:38 +0530

    <CREATE-JOIN> Added pre-commit hooks

---
+++ scripts/setup.sh
@@ -0,0 +1,10 @@
+# run this from the root directory 
+
+cp ./scripts/pre-commit ./.git/hooks/
+chmod +x ./.git/hooks/pre-commit
+
+npm install
+
+# that's it for now
+# let's see if we can make it better
+
