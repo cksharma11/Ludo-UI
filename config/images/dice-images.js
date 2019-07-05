efe9621ca43f1a28c07f3522a58e4bbd8b0b37a9
Chandan Kumar <22887527+cksharma11@users.noreply.github.com>
2019-07-06 00:18:35 +0530

    <00 - Game Page> Added Dice images to config

---
+++ config/images/dice-images.js
@@ -0,0 +1,10 @@
+const diceImages = {
+  1: 'https://upload.wikimedia.org/wikipedia/commons/0/09/Dice-1.svg',
+  2: 'https://upload.wikimedia.org/wikipedia/commons/3/34/Dice-2.svg',
+  3: 'https://upload.wikimedia.org/wikipedia/commons/thumb/c/ca/Dice-3.svg/557px-Dice-3.svg.png',
+  4: 'https://upload.wikimedia.org/wikipedia/commons/thumb/1/16/Dice-4.svg/557px-Dice-4.svg.png',
+  5: 'https://upload.wikimedia.org/wikipedia/commons/thumb/d/dc/Dice-5.svg/557px-Dice-5.svg.png',
+  6: 'https://upload.wikimedia.org/wikipedia/commons/thumb/1/14/Dice-6.svg/557px-Dice-6.svg.png'
+};
+
+export default diceImages;
